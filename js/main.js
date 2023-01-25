adfc1f1e2a336b95ae76e0ba7f0f58a160fb0a33
Andres <andres.tobonz@upb.edu.co>
2023-01-24 23:48:38 -0500

    Primer commit/ Se agrega el proyecto

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 window.onload = init;
 
 function init(){
-    alert("Al entrar a este sitio aceptas las cookies");
+    alert("Al entrar a este sitio aceptas las cookies y no nos hacemos responsables de tu informacion privada :)");
 }
